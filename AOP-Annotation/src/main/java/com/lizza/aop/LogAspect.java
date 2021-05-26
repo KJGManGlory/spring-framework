6e80b97f8759af548ac45e81601bdb32e8d597e1
lizza <liutao_glory@126.com>
2021-05-26 10:22:12 +0800

    注解 aop

---
+++ AOP-Annotation/src/main/java/com/lizza/aop/LogAspect.java
@@ -0,0 +1,33 @@
+package com.lizza.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * @Desc:
+ * @author: lizza.liu
+ * @date: 2021-05-25
+ */
+@Aspect
+@Component
+public class LogAspect {
+
+    @After("execution(* com.lizza.dao..*.delete*(..)) or" +
+            "execution(* com.lizza.dao..*.save*(..))")
+    public void daoLog(JoinPoint joinPoint) {
+        try {
+            System.out.println(">>>>>>Class Method: " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName()
+                            + ", Args: {}" + Arrays.toString(joinPoint.getArgs()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void print() {
+        System.out.println("日志记录");
+    }
+}
---
+++ AOP-Annotation/src/main/java/com/lizza/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.lizza.dao;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Desc:
+ * @author: lizza.liu
+ * @date: 2021-05-25
+ */
+@Service
+public class UserDao {
+
+
+    public void delete() {
+        System.out.println("删除用户");
+    }
+}
